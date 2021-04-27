66e31a0ab7814dd472f3dec5d306e79db365f162
Ruben Diaz <rudiaz@microsoft.com>
2021-04-27 15:27:28 -0700

    Module 9 work

---
+++ module9_solution/src/categories.controller.js
@@ -0,0 +1,10 @@
+(function () {
+    'use strict';
+    angular.module('data')
+    .controller('CategoriesController', CategoriesController);
+    CategoriesController.$inject = ['categories'];
+    function CategoriesController(categories) {
+      var $ctrl = this;
+      $ctrl.items = categories;
+    }
+})();
